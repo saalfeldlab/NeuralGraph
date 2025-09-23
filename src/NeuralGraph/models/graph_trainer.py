67fb7daaff17e6474ddd4a54b7bb2103a7fad6fe
cedric <allierc@janelia.hhmi.org>
2025-09-23 16:11:56 -0400

    issue import MPM_P2G

--- src/NeuralGraph/models/graph_trainer.py
+++ src/NeuralGraph/models/graph_trainer.py
@@ -370,7 +370,7 @@ def data_train_signal(config, erase, best_model, device):
         else:
             Niter = int(n_frames * data_augmentation_loop // batch_size * 0.2 // max(recursive_loop, 1))
 
-        plot_frequency = int(Niter // 200)
+        plot_frequency = int(Niter // 5)
         print(f'{Niter} iterations per epoch')
         logger.info(f'{Niter} iterations per epoch')
         print(f'plot every {plot_frequency} iterations')
@@ -1596,12 +1596,6 @@ def data_train_zebra(config, erase, best_model, device):
         plt.close()
 
 
-
-
-
-
-                                                    
-                                            
 
 
 
