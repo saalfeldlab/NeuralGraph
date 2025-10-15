6e03ceb7ab4a40f75cbdbde05675d2a1477cd2cd
cedric <allierc@janelia.hhmi.org>
2025-10-15 10:53:06 -0400

    training 62_2 with visual NNR

--- GNN_Main.py
+++ GNN_Main.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
             best_model = None
     else:
         best_model = None 
-        task = 'generate'  # 'generate', 'train', 'test'
+        task = 'train'  # 'generate', 'train', 'test'
 
         # config_list = ['signal_CElegans_d2', 'signal_CElegans_d2a', 'signal_CElegans_d3', 'signal_CElegans_d3a', 'signal_CElegans_d3b']
         # config_list = ['signal_CElegans_c14_4']
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 
         # config_list = ['fly_N9_51_15', 'fly_N9_51_16', 'fly_N9_51_17']
 
-        config_list = ['fly_N9_62_1']
+        config_list = ['fly_N9_62_2']
 
         # config_list = ['signal_N11_5_1_3']
 
