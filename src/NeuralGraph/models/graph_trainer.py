d1a84220323e6aa5249f3fa0b722ecb16e35c8a5
cedric <allierc@janelia.hhmi.org>
2025-10-15 10:56:52 -0400

    fix plot in graph_trainer_flyvis

--- src/NeuralGraph/models/graph_trainer.py
+++ src/NeuralGraph/models/graph_trainer.py
@@ -949,7 +949,7 @@ def data_train_flyvis(config, erase, best_model, device):
         else:
             Niter = int(n_frames * data_augmentation_loop // batch_size * 0.2)
 
-        plot_frequency = int(Niter // 20)
+        plot_frequency = int(Niter // 10)
         print(f'{Niter} iterations per epoch')
         logger.info(f'{Niter} iterations per epoch')
         print(f'plot every {plot_frequency} iterations')
